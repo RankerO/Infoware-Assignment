e4fe8d854aff05cb76eff56fe67701b469fba16d
Ankit pandey <91595780+RankerO@users.noreply.github.com>
2023-04-12 23:34:14 +0530

    Delete SpringSecurityInitializer.java

--- employee/src/main/java/com/Infoware/employee/spring/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-//package com.Infoware.employee.spring;
-//
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-//    //no code needed
-//}
